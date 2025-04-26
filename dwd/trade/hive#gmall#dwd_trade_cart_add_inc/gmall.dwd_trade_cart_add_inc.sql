ef9ef669e51fc956c5de786b705e594ad5f56263
mhstudy <153063151+mhstudy@users.noreply.github.com>
2025-04-26 16:57:24 +0800

    Update gmall.dwd_trade_cart_add_inc.sql

--- dwd/trade/hive#gmall#dwd_trade_cart_add_inc/gmall.dwd_trade_cart_add_inc.sql
+++ dwd/trade/hive#gmall#dwd_trade_cart_add_inc/gmall.dwd_trade_cart_add_inc.sql
@@ -1,3 +1,22 @@
+-- 首日加载脚本
+set hive.exec.dynamic.partition.mode=nonstrict;
+
+insert overwrite table ${APP}.dwd_trade_cart_add_inc partition (dt)
+select data.id,
+       data.user_id,
+       data.sku_id,
+       date_format(data.create_time, 'yyyy-MM-dd') date_id,
+       data.create_time,
+       data.sku_num,
+       date_format(data.create_time, 'yyyy-MM-dd')
+from ${APP}.ods_cart_info_inc
+where dt = '$do_date'
+  and type = 'bootstrap-insert'
+
+
+;
+
+-- 每日加载
 insert overwrite table ${APP}.dwd_trade_cart_add_inc partition (dt = '$do_date')
 select data.id,
        data.user_id,
